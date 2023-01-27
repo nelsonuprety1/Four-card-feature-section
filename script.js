0e0b351525994db79fe8f70296620de33550696d
Nelson Uprety <nelsonuprety3@gmail.com>
2023-01-27 01:22:19 +0000

    Dark mode functionality

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const btn = document.getElementById('dark-mode-btn');
-let isDarkMode = false;
 
 btn.addEventListener('click', () => {
-  if (isDarkMode) {
-    document.body.style.backgroundColor = 'white';
-    btn.innerText = 'Dark Mode';
-    isDarkMode = false;
-  } else {
-    document.body.style.backgroundColor = 'black';
-    btn.innerText = 'Light Mode';
-    isDarkMode = true;
-  }
+  document.body.classList.toggle('dark-mode');
+  document
+    .querySelectorAll('.text-light')
+    .forEach(el => el.classList.toggle('text-light'));
 });
+
+// const btn = document.getElementById('dark-mode-btn');
+// let isDarkMode = false;
+
+// btn.addEventListener('click', () => {
+//   if (isDarkMode) {
+//     document.body.style.backgroundColor = 'white';
+//     btn.innerText = 'Dark Mode';
+//     isDarkMode = false;
+//   } else {
+//     document.body.style.backgroundColor = 'black';
+//     btn.innerText = 'Light Mode';
+//     isDarkMode = true;
+//   }
+// });
